1b5b6867b908a53183d621973c428035be885b9c
YiYang-github <yanggnay@mail.ustc.edu.cn>
2024-10-03 16:08:48 +0800

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,47 @@
 from setuptools import setup, find_packages
+import setuptools
+import requests
+import re
+import os
 
-with open('requirements.txt') as f:
-    required_packages = f.read().splitlines()
 
-setup(
-    name='tonelab',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=required_packages,
-    author='Yi Yang',
-    author_email='yanggnay@mail.ustc.edu.cn',
-    description='Platform designed for lightweight documentation and quantitative analysis in Sino-Tibetan tonal languages',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/YiYang-github/ToneLab',
-    license='MIT'  # Specify your license here
-)
+def upload():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+    with open('requirements.txt') as f:
+        required = f.read().splitlines()
+
+    setuptools.setup(
+        name='tonelab',
+        version='0.2',
+        author='Yi Yang',
+        author_email='yanggnay@mail.ustc.edu.cn',
+        description='Platform designed for lightweight documentation and quantitative analysis in Sino-Tibetan tonal languages',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/YiYang-github/ToneLab',
+        packages=setuptools.find_packages(),
+        data_files=["requirements.txt"], 
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering",
+        ],
+        python_requires='>=3.6',
+        install_requires=required,
+    )
+
+
+
+def main():
+    try:
+        upload()
+        print("Upload success")
+    except Exception as e:
+        raise Exception("Upload package error", e)
+
+
+if __name__ == '__main__':
+    main()
